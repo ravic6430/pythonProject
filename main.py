4665f7c385a48546f029f64b77ba24a67980e0b0
MouseHouse <mh@9209.ru>
2020-11-15 21:43:14 +0500

    Новая версия

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# как то так
+
 import os
+
 
 
 # Press the green button in the gutter to run the script.
