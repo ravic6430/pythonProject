88a7d91224e36f7f6b091fffd3f15ec2791cb7bf
MouseHouse <mh@9209.ru>
2020-11-15 21:31:02 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    os.system('python3 python_mysql_connect.py')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
