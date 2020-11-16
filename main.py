3b809114a96f7137535d55a1221adeb85ac37ccc
ravic6430 <ravic@6430.ru>
2020-11-16 12:43:00 +0500

    win

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-# как то так непонятно
+from mysql.connector import MySQLConnection, Error
+from python_mysql_dbconfig import read_db_config
 
-import os
+def query_with_fetchone():
+    try:
+        dbconfig = read_db_config()
+        conn = MySQLConnection(**dbconfig)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM wp_users")
 
+        row = cursor.fetchone()
 
+        while row is not None:
+            #print(row)
 
-# Press the green button in the gutter to run the script.
+            print("{0} {1} {2}".format(row[0], row[1], row[2]))
+
+            row = cursor.fetchone()
+
+    except Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
 if __name__ == '__main__':
-    os.system('python3 python_mysql_connect.py')
+    query_with_fetchone()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
--- python_mysql_connect.py
+++ python_mysql_connect.py
@@ -19,25 +19,8 @@ def connect():
         print(error)
 
     finally:
-
-        with conn:
-
-            cur = conn.cursor()
-            cur.execute("SELECT * FROM wp_users")
-
-            rows = cur.fetchall()
-
-            for row in rows:
-                print("{0} {1} {2}".format(row[0], row[1], row[2]))
-
-
-
-
-
-
-
-        #conn.close()
+        conn.close()
         print('Connection closed.')
 
-if __name__ == '__main__':
-    connect()
+    if __name__ == '__main__':
+        connect()
